5b88d21f56b64f33f474bea7387d93682252f972
Rabi03 <islamrabi02@gmail.com>
2023-09-20 14:52:03 +0600

    update

--- controllers/UserControllers.js
+++ controllers/UserControllers.js
@@ -16,7 +16,7 @@ exports.registerUser = async (req, res, next) => {
       crop: "scale",
     });
 
-    const { name, email, password } = req.body;
+    const { name, email, password,role } = req.body;
     console.log(req.body)
 
     const user = await User.findOne({ email });
@@ -34,6 +34,7 @@ exports.registerUser = async (req, res, next) => {
         name,
         email,
         password,
+        role,
         avatar: {
           public_id: result.public_id,
           url: result.secure_url,
