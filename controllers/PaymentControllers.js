e71d30a71e3b4a2fb02bc5fcc7946b0e7fd00f2e
Rabi03 <islamrabi02@gmail.com>
2023-10-28 10:58:51 +0600

    update

--- controllers/PaymentControllers.js
+++ controllers/PaymentControllers.js
@@ -5,10 +5,6 @@ const axios = require("axios");
 const uuid = require('uuid')
 const Order = require('../modals/order')
 
-const SSLCommerzPayment = require('sslcommerz-lts')
-const store_id = 'sust63616e0a0b0a8'
-const store_passwd = 'sust63616e0a0b0a8@ssl'
-const is_live = false //true for live, false for sandbox
 
 exports.processPayment = async (req, res, next) => {
   const tranId = uuid.v4();
@@ -16,7 +12,11 @@ exports.processPayment = async (req, res, next) => {
   const newOrder = await Order.create({ ...order, user: user._id })
 
   try {
-    const data = {
+    const val = await axios.post(
+      "https://sandbox.aamarpay.com/jsonpost.php",
+      {
+        store_id: "aamarpaytest",
+        signature_key: "dbb74894e82415a2f7ff0ec3a97e4183",
         cus_name: user?.name ? user?.name : "Rabi Islam",
         cus_email: user?.email ? user?.email : 'islamrabi02@gmail.com',
         cus_phone: shippingInfo?.phoneNo ? shippingInfo?.phoneNo : '01748627513',
@@ -24,32 +24,22 @@ exports.processPayment = async (req, res, next) => {
         cus_add2: shippingInfo?.city ? shippingInfo?.city : "Dhaka",
         cus_city: shippingInfo?.city ? shippingInfo?.city : "Dhaka",
         cus_country: shippingInfo?.country ? shippingInfo?.country : "Dhaka",
-        total_amount: totalPrice,
+        amount: totalPrice,
         tran_id: tranId,
         currency: "BDT",
         success_url: `https://walmart-backend.vercel.app/api/v1/payment/callback/${newOrder._id}`,
         fail_url: `https://walmart-backend.vercel.app/api/v1/payment/callback/${newOrder._id}`,
         cancel_url: `https://walmart-backend.vercel.app/api/v1/payment/callback/${newOrder._id}`,
-        ship_name: user?.name ? user?.name : "Rabi Islam",
-        ship_add1: 'Dhaka',
-        ship_add2: 'Dhaka',
-        ship_city: 'Dhaka',
-        ship_state: 'Dhaka',
-        ship_postcode: 1000,
-        ship_country: 'Bangladesh',
-        
-      
+        desc: "Package Bill",
+        type: "json"
+      }, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
     }
-    const sslcz = new SSLCommerzPayment(store_id, store_passwd, is_live)
-    sslcz.init(data).then(apiResponse => {
-        // Redirect the user to payment gateway
-        let GatewayPageURL = apiResponse.GatewayPageURL
-        console.log(GatewayPageURL)
-        
-        res.status(200).json({ url: GatewayPageURL });
-    });
-    
-    
+    );
+    console.log(val.data)
+    res.status(200).json({ url: val.data.payment_url });
   } catch (err) {
     console.log(err);
   }
